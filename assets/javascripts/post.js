14dbc7d8e7800bf5ae43f0ac735ee497c934944f
Voltstro <me@voltstro.dev>
2019-05-19 13:07:17 +1000

    Updated post.js

--- assets/javascripts/post.js
+++ assets/javascripts/post.js
@@ -1,34 +1,25 @@
-var postName;
-var postDate;
-var postAuthor;
-
-function setup() {
+function setupPage(infoPID, shareDivID) {
+	console.log("Setting up.");
+	//Get post data
 	$.getJSON("post.json", function(data) {
-		postName = data.PostName;
-		postDate = data.PostUploadedDate;
-		postAuthor = data.PostAuthor;
+		//Setup date/author text
+		document.getElementById(infoPID).innerHTML = data.PostUploadedDate + " - " + data.PostAuthor;
+
+		//Setup share links
+		var element = document.getElementById(shareDivID);
+
+		var shareTitle = document.createElement("p");
+		shareTitle.innerHTML = "Share:";
+
+		var formatTitle = data.PostName.replace(" ", "%20");
+
+		var shareBtns = document.createElement("p");
+		shareBtns.innerHTML = "<a class='fa fa-reddit' href='http://reddit.com/submit?url=" + window.location.href + "&amp;title=" + formatTitle + "'></a> " +
+		"<a class='fa fa-facebook' href='http://www.facebook.com/sharer.php?u=" + window.location.href + "'></a> " +
+		"<a class='fa fa-twitter' href='https://twitter.com/share?url=" + window.location.href + "&amp;text=" + formatTitle + "'></a> " +
+		"<a class='fa fa-envelope-open' href='mailto:?Subject=" + formatTitle + "&amp;Body=" + window.location.href + "'></a>";
+
+		element.appendChild(shareTitle);
+		element.appendChild(shareBtns);
 	});
-}
-
-function getPostInfo(infoPID) {
-	document.getElementById(infoPID).innerHTML = postDate + " - " + postAuthor;
-}
-
-function setShareLinks(shareDivID) {
-	var element = document.getElementById(shareDivID);
-
-	var shareTitle = document.createElement("p");
-	shareTitle.innerHTML = "Share:";
-
-	var formatTitle = postName.replace(" ", "%20");
-
-	var shareBtns = document.createElement("p");
-	shareBtns.innerHTML = "<a class='fa fa-reddit' href='http://reddit.com/submit?url=" + window.location.href + "&amp;title=" + formatTitle + "'></a> " +
-	"<a class='fa fa-facebook' href='http://www.facebook.com/sharer.php?u=" + window.location.href + "'></a> " +
-	"<a class='fa fa-twitter' href='https://twitter.com/share?url=" + window.location.href + "&amp;text=" + formatTitle + "'></a> " +
-	"<a class='fa fa-envelope-open' href='mailto:?Subject=" + formatTitle + "&amp;Body=" + window.location.href + "'></a>";
-
-	element.appendChild(shareTitle);
-	element.appendChild(shareBtns);
-
 }
