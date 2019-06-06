23cc73c5cf85d6910d68ef16629fbbbdaa38c4e7
Voltstro <me@voltstro.dev>
2019-06-06 19:13:20 +1000

    Fixed blogsystem.js with its underdefined message

--- assets/javascripts/blogsystem.js
+++ assets/javascripts/blogsystem.js
@@ -39,9 +39,11 @@ function getAllBlogs(allBlogsDivID) {
 	$.getJSON("posts.json", function(data) {
 		
 		for(var i=0;i<data.allPosts.length;i++) {
+			var postLoc = data.allPosts[i];
+
 			$.getJSON("posts/" + data.allPosts[i] + "/post.json", function(post) {
 				var title = document.createElement("h2");
-				title.innerHTML = " <a href='posts/" + data.allPosts[i] + "/'>" + post.PostName + "</a>";
+				title.innerHTML = " <a href='posts/" + postLoc + "/'>" + post.PostName + "</a>";
 				
 				var info = document.createElement("h4");
 				info.innerHTML = post.PostUploadedDate + " - " + post.PostAuthor;
